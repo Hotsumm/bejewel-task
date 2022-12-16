39d73dff3b207c52c10515da0b06361ed614f2b9
Yeolha Sung <tjddufgk@gmail.com>
2022-12-17 00:28:31 +0900

    feat: add react-query provider in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import Layout from './components/layout/Layout';
 import GlobalStyles from './styles/GlobalStyles';
 import Router from './Router';
 
 export default function App() {
+  const queryClient = new QueryClient();
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyles />
       <Layout>
         <Router />
       </Layout>
-    </>
+    </QueryClientProvider>
   );
 }
